e70b83adb698c440886d19ac5d195f45a6f541d4
gaspard <gaspard@gmail.com>
2017-08-10 22:19:36 +0200

    count: add words to a dictionary and count occurences

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ function cut(string) {
   return string.split(/[\s,.\[\]$\?¿\!¡'"()+-]+/);
 }
 
-
-function count(words, dictionary) {
+/*
+ * count
+ * @param {Array} words
+ * @param {Map} dictionary with {word: occurence}
+ * @return {Map} dictionary
+ */
+function count(words, dictionary = new Map()) {
+  words.forEach((word) => {
+    const w = word.toLowerCase();
+    if (dictionary.has(w)) {
+      const occurence = dictionary.get(w);
+      dictionary.set(w, occurence + 1);
+    }
+    else dictionary.set(w, 1);
+  });
   return dictionary;
 }
 
